f7b3eb817d1d2e2b5ee2a415d78c2c3359f76e91
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-10-21 04:19:15 +0000

    mostly one

--- app.rb
+++ app.rb
@@ -53,10 +53,14 @@ get("/dice/5/4") do
   erb(:five_four)
 end
 
-get("/dynamic/:number_of_dice/6") do
+get("/dice/:number_of_dice/:how_many_sides") do
+  @num_dice = params.fetch("number_of_dice").to_i
+  
+  @sides = params.fetch("how_many_sides").to_i
+
   @rolls = []
 
-  50.times do
+  @num_dice.times do
     die = rand(1..6)
 
     @rolls.push(die)
